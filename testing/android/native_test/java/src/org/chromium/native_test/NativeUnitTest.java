52b0e06a8840cea92ac0b55be2f769f6bfd83697
Mohannad Farrag <aymanm@google.com>
2023-01-31 10:10:32 +0000

    cronet: Implement Cronet Test-Runner
    * This is a simple implementation of Chromium's test running infrastructure but in Java, using tradefed classes in order to be compatible with Android's infra.
    * Implemented native-code coverage dumping for instrumented tests.
      * In order for an APK to dump its native code coverage. Signal 37 must be recieved before the application quits. This changes makes sure that after the tests have finished running and the control is back
      to java, Signal 37 is sent to the main process and wait 60 seconds until dumping of the coverage data has finished.
      * Added an option to enable/disable dumping of native code coverage.
    * Chromium used NativeLibraries.java class to specify which libraries to load for testing. This class is generated by a genrule which is inefficient for Cronet on AOSP. Instead this was changed to recieve the name of the
    library to test using an instrumentation flag.
    * Collect logs that are produced by gtest [Json ouput and stdout] into specific files to determine which tests have passed/failed.
    
    Test: atest cronet_unittests_tester
    Change-Id: I0d7b8af5bbf4806822d86989e302fe2aa45e0823

--- testing/android/native_test/java/src/org/chromium/native_test/NativeUnitTest.java
+++ testing/android/native_test/java/src/org/chromium/native_test/NativeUnitTest.java
@@ -19,7 +19,9 @@ import org.chromium.build.NativeLibraries;
  */
 public class NativeUnitTest extends NativeTest {
     private static final String TAG = "NativeTest";
-    private static final String[] LIBRARIES = {"cronet_aml_components_cronet_android_cronet_unittests_android__library"};
+
+    private static final String LIBRARY_UNDER_TEST_NAME =
+            "org.chromium.native_test.NativeTestInstrumentationTestRunner.LibraryUnderTest";
     private static class NativeUnitTestLibraryLoader extends LibraryLoader {
         static void setLibrariesLoaded() {
             LibraryLoader.setLibrariesLoadedForNativeTests();
@@ -46,17 +48,20 @@ public class NativeUnitTest extends NativeTest {
         // For NativeActivity based tests,
         // dependency libraries must be loaded before NativeActivity::OnCreate,
         // otherwise loading android.app.lib_name will fail
-        loadLibraries();
+        String libraryToLoad = activity.getIntent().getStringExtra(LIBRARY_UNDER_TEST_NAME);
+        if (libraryToLoad != null) {
+            loadLibrary(libraryToLoad);
+        } else {
+            Log.e(TAG, "No Library provided for native tests! Exiting");
+            activity.finish();
+        }
     }
 
-    private void loadLibraries() {
+    private void loadLibrary(String library) {
         LibraryLoader.setEnvForNative();
-        Log.i(TAG, "LOADING LIBRARIES");
-        for (String library : LIBRARIES) {
-            Log.i(TAG, "loading: %s", library);
-            System.loadLibrary(library);
-            Log.i(TAG, "loaded: %s", library);
-        }
+        Log.i(TAG, "loading: %s", library);
+        System.loadLibrary(library);
+        Log.i(TAG, "loaded: %s", library);
         NativeUnitTestLibraryLoader.setLibrariesLoaded();
     }
 }
