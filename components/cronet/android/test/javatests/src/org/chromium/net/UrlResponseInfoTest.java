89992f075a454a4edc920a94f582f339a191cb24
My Name <colibie@google.com>
2023-01-31 21:50:19 +0000

    Preliminary setup for cronet's java mts
    Test: atest NetHttpTests
    Bug: 267353182
    Change-Id: I94da111c4a5fe69ef36683f0f97279e91a806e76

--- components/cronet/android/test/javatests/src/org/chromium/net/UrlResponseInfoTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/UrlResponseInfoTest.java
@@ -5,7 +5,7 @@
 package org.chromium.net;
 
 import android.net.http.UrlResponseInfo;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import androidx.test.filters.SmallTest;
 
