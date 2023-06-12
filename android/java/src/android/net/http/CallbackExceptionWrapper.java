b70af4f9a38e711886ae8a9ba521b237a4b6d120
Stefano Duo <stefanoduo@google.com>
2023-06-12 16:46:59 +0100

    Introduce wrapper logic for android.net.http -> org.chromium.net
    Bug: 288886953
    Change-Id: Ia7545c42800b923ed29fbec45006311f3caf3cf1

---
+++ android/java/src/android/net/http/CallbackExceptionWrapper.java
@@ -0,0 +1,8 @@
+package android.net.http;
+
+public class CallbackExceptionWrapper extends android.net.http.CallbackException {
+
+    protected CallbackExceptionWrapper(org.chromium.net.CallbackException e) {
+        super(e.getMessage(), ExceptionTranslationUtils.translateNestedException(e.getCause()));
+    }
+}
