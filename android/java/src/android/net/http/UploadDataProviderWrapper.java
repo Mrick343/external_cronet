81a461289054ea4df12488c5450a2a4ab932be4b
Stefano Duo <stefanoduo@google.com>
2023-07-04 15:44:12 +0100

    Add UploadDataProviderWrapper
    android.net.http.UploadDataProvider#{read, rewind} can throw
    IOExceptions. These should not be transalted, as they are thrown from
    users' callback code.
    What needs to be translated is the org.chromium.net.CallbackException
    that will be passed to android.net.http.UrlRequest.Callback#onFailed.
    
    Bug: 288886953
    Change-Id: I3232c2c40f8445dd5dae9d88edef591fab4a52aa

---
+++ android/java/src/android/net/http/UploadDataProviderWrapper.java
@@ -0,0 +1,31 @@
+package android.net.http;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+@SuppressWarnings("Override")
+public class UploadDataProviderWrapper extends org.chromium.net.UploadDataProvider {
+
+    private final android.net.http.UploadDataProvider backend;
+
+    public UploadDataProviderWrapper(android.net.http.UploadDataProvider backend) {
+        this.backend = backend;
+    }
+
+    @Override
+    public long getLength() throws IOException {
+        return backend.getLength();
+    }
+
+    @Override
+    public void read(org.chromium.net.UploadDataSink sink, ByteBuffer buffer) throws IOException {
+        UploadDataSinkWrapper wrappedSink = new UploadDataSinkWrapper(sink);
+        backend.read(wrappedSink, buffer);
+    }
+
+    @Override
+    public void rewind(org.chromium.net.UploadDataSink sink) throws IOException {
+        UploadDataSinkWrapper wrappedSink = new UploadDataSinkWrapper(sink);
+        backend.rewind(wrappedSink);
+    }
+}
