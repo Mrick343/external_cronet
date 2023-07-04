388be7568ef3c49be50f4fc2704c2af65a9ad8ab
Stefano Duo <stefanoduo@google.com>
2023-07-04 15:47:43 +0100

    Add BidirectionalStreamWrapper
    Bug: 288886953
    Change-Id: Ie5f589342b3f39c225efbdf32001c9a91a0cff40

---
+++ android/java/src/android/net/http/BidirectionalStreamWrapper.java
@@ -0,0 +1,82 @@
+package android.net.http;
+
+import java.nio.ByteBuffer;
+
+public class BidirectionalStreamWrapper extends android.net.http.BidirectionalStream {
+
+    private final org.chromium.net.BidirectionalStream backend;
+
+    BidirectionalStreamWrapper(org.chromium.net.BidirectionalStream backend) {
+        this.backend = backend;
+    }
+
+    @Override
+    public void start() {
+        backend.start();
+    }
+
+    @Override
+    public void read(ByteBuffer buffer) {
+        backend.read(buffer);
+    }
+
+    @Override
+    public void write(ByteBuffer buffer, boolean endOfStream) {
+        backend.write(buffer, endOfStream);
+    }
+
+    @Override
+    public void flush() {
+        backend.flush();
+    }
+
+    @Override
+    public void cancel() {
+        backend.cancel();
+    }
+
+    @Override
+    public boolean isDone() {
+        return backend.isDone();
+    }
+
+    @Override
+    public boolean isDelayRequestHeadersUntilFirstFlushEnabled() {
+        return backend.isDelayRequestHeadersUntilFirstFlushEnabled();
+    }
+
+    @Override
+    public int getPriority() {
+        return backend.getPriority();
+    }
+
+    @Override
+    public String getHttpMethod() {
+        return backend.getHttpMethod();
+    }
+
+    @Override
+    public boolean hasTrafficStatsTag() {
+        return backend.hasTrafficStatsTag();
+    }
+
+    @Override
+    public int getTrafficStatsTag() {
+        return backend.getTrafficStatsTag();
+    }
+
+    @Override
+    public boolean hasTrafficStatsUid() {
+        return backend.hasTrafficStatsUid();
+    }
+
+    @Override
+    public int getTrafficStatsUid() {
+        return backend.getTrafficStatsUid();
+    }
+
+    @Override
+    public android.net.http.HeaderBlock getHeaders() {
+        return new HeaderBlockWrapper(backend.getHeaders());
+    }
+}
