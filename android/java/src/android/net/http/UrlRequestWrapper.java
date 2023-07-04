c0cf256bc208c1c3f93ee2ad2f79a10a4351c82c
Stefano Duo <stefanoduo@google.com>
2023-07-04 15:41:13 +0100

    Add UrlRequestWrapper
    getHeaders and getStatus are the only methods handling Cronet-specific
    types. The "lifetime" of these objects is not specified anywhere, so
    play it safe and always use a new wrapper.
    
    Bug: 288886953
    Change-Id: I02281b73dcb9f3a4aaad249f99c4a338e3f78ba9

---
+++ android/java/src/android/net/http/UrlRequestWrapper.java
@@ -0,0 +1,88 @@
+package android.net.http;
+
+import java.nio.ByteBuffer;
+
+class UrlRequestWrapper extends android.net.http.UrlRequest {
+
+    private final org.chromium.net.UrlRequest backend;
+
+    public UrlRequestWrapper(org.chromium.net.UrlRequest backend) {
+        this.backend = backend;
+    }
+
+    @Override
+    public void start() {
+        backend.start();
+    }
+
+    @Override
+    public void followRedirect() {
+        backend.followRedirect();
+    }
+
+    @Override
+    public void read(ByteBuffer buffer) {
+        backend.read(buffer);
+    }
+
+    @Override
+    public void cancel() {
+        backend.cancel();
+    }
+
+    @Override
+    public boolean isDone() {
+        return backend.isDone();
+    }
+
+    @Override
+    public void getStatus(android.net.http.StatusListener listener) {
+        backend.getStatus(new UrlRequestStatusListenerWrapper(listener));
+    }
+
+    @Override
+    public String getHttpMethod() {
+        return backend.getHttpMethod();
+    }
+
+    @Override
+    public android.net.http.HeaderBlock getHeaders() {
+        org.chromium.net.UrlResponseInfo.HeaderBlock headers = backend.getHeaders();
+        return new HeaderBlockWrapper(headers);
+    }
+
+    @Override
+    public boolean isCacheDisabled() {
+        return backend.isCacheDisabled();
+    }
+
+    @Override
+    public boolean isDirectExecutorAllowed() {
+        return backend.isDirectExecutorAllowed();
+    }
+
+    @Override
+    public int getPriority() {
+        return backend.getPriority();
+    }
+
+    @Override
+    public boolean hasTrafficStatsTag() {
+        return backend.hasTrafficStatsTag();
+    }
+
+    @Override
+    public int getTrafficStatsTag() {
+        return backend.getTrafficStatsTag();
+    }
+
+    @Override
+    public boolean hasTrafficStatsUid() {
+        return backend.hasTrafficStatsUid();
+    }
+
+    @Override
+    public int getTrafficStatsUid() {
+        return backend.getTrafficStatsUid();
+    }
+}
