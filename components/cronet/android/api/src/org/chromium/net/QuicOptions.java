211a972a7b26d19943916d29b7505f4e282ee067
Stefano Duo <stefanoduo@google.com>
2023-07-12 11:48:33 +0100

    Add QuicOptionsWrapper
    QuicOptionsWrapper forwards options that exists only in the
    org.chromium.net API to a default-constructed
    org.chromium.net.DnsOption. APIs which are also present in the
    android.net.http.DnsOption API do instead get forwarded to the wrapped
    backend.
    
    This requires changing org.chromium.net.QuicOptions' constructor to
    public to allow a direct instantiation of a default constructed
    org.chromium.net.QuicOptions.
    
    Bug: 290617849
    Change-Id: Ia3f2a34b9a7fa19f6bd3af8a5281edad4cf158dd

--- components/cronet/android/api/src/org/chromium/net/QuicOptions.java
+++ components/cronet/android/api/src/org/chromium/net/QuicOptions.java
@@ -57,7 +57,7 @@ public class QuicOptions {
 
     private final Set<String> mExtraQuicheFlags;
 
-    QuicOptions(Builder builder) {
+    public QuicOptions(Builder builder) {
         this.mQuicHostAllowlist =
                 Collections.unmodifiableSet(new LinkedHashSet<>(builder.mQuicHostAllowlist));
         this.mEnabledQuicVersions =
