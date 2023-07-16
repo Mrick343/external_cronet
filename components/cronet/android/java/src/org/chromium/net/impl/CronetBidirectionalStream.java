01db5af791df3a1010ca83fdbcdacc1342c0d755
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2023-07-16 02:34:30 +0000

    Revert "Fix BidirectionalStream getters"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/a37e6735-59bb-47f0-a1fa-5d7f6311b0ef). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ie0110f67a2affcaf4772c008fbc2463d43540401

--- components/cronet/android/java/src/org/chromium/net/impl/CronetBidirectionalStream.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetBidirectionalStream.java
@@ -4,12 +4,6 @@
 
 package org.chromium.net.impl;
 
-import static org.chromium.net.BidirectionalStream.Builder.STREAM_PRIORITY_IDLE;
-import static org.chromium.net.BidirectionalStream.Builder.STREAM_PRIORITY_LOWEST;
-import static org.chromium.net.BidirectionalStream.Builder.STREAM_PRIORITY_LOW;
-import static org.chromium.net.BidirectionalStream.Builder.STREAM_PRIORITY_MEDIUM;
-import static org.chromium.net.BidirectionalStream.Builder.STREAM_PRIORITY_HIGHEST;
-
 import androidx.annotation.IntDef;
 import androidx.annotation.VisibleForTesting;
 
@@ -99,7 +93,6 @@ public class CronetBidirectionalStream extends ExperimentalBidirectionalStream {
     private final int mInitialPriority;
     private final String mInitialMethod;
     private final String mRequestHeaders[];
-    private final UrlResponseInfo.HeaderBlock mRequestHeaderBlock;
     private final boolean mDelayRequestHeadersUntilFirstFlush;
     private final Collection<Object> mRequestAnnotations;
     private final boolean mTrafficStatsTagSet;
@@ -257,7 +250,6 @@ public class CronetBidirectionalStream extends ExperimentalBidirectionalStream {
         mExecutor = executor;
         mInitialMethod = httpMethod;
         mRequestHeaders = stringsFromHeaderList(requestHeaders);
-        mRequestHeaderBlock = new UrlResponseInfoImpl.HeaderBlockImpl(requestHeaders);
         mDelayRequestHeadersUntilFirstFlush = delayRequestHeadersUntilNextFlush;
         mPendingData = new LinkedList<>();
         mFlushData = new LinkedList<>();
@@ -301,7 +293,7 @@ public class CronetBidirectionalStream extends ExperimentalBidirectionalStream {
     }
 
     @Override
-    public UrlResponseInfo.HeaderBlock getHeaders() {
+    public HeaderBlock getHeaders() {
         return mRequestHeaderBlock;
     }
 
