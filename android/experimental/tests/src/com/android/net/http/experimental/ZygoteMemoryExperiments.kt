80f50125bc1831923979133709b350b936e4e0c8
Stefano Duo <stefanoduo@google.com>
2023-08-08 17:09:12 +0100

    Add HttpEngine to testIsolatedService
    Change-Id: Ia0eb9e1f848e667cd54cc8cf910b85211e96fe82

--- android/experimental/tests/src/com/android/net/http/experimental/ZygoteMemoryExperiments.kt
+++ android/experimental/tests/src/com/android/net/http/experimental/ZygoteMemoryExperiments.kt
@@ -17,6 +17,12 @@
 package com.android.net.http.experimental
 
 import android.content.Intent
+import java.nio.ByteBuffer;
+import android.net.http.HttpEngine
+import java.util.concurrent.Executors
+import android.net.http.UrlRequest
+import android.net.http.HttpException
+import android.net.http.UrlResponseInfo
 import android.content.ComponentName
 import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -45,7 +51,7 @@ class ZygoteMemoryExperiments {
         // Clean up output after previous test run.
         val perfettoConfig = "/data/misc/perfetto-traces/perfetto-config.txt"
         val traceOut = "/data/misc/perfetto-traces/trace-${testName.methodName}"
-        runShellCommand("perfetto -c $perfettoConfig --txt -o $traceOut")
+        runShellCommand("perfetto -D -c $perfettoConfig --txt -o $traceOut")
     }
 
     @Before
@@ -56,13 +62,20 @@ class ZygoteMemoryExperiments {
     @After
     fun tearDown() {
         // HACK: Wait for trace to complete
-        Thread.sleep(5000)
+        Thread.sleep(7000)
     }
 
     @Test
     fun testIsolatedService() {
         val serviceIntent = Intent(context, EmptyIsolatedService::class.java)
         context.startService(serviceIntent)
+        val mEngineBuilder = HttpEngine.Builder(context);
+        val mEngine = mEngineBuilder.build()
+        val mCallback = MyCallback()
+        val builder =
+                mEngine.newUrlRequestBuilder("www.google.com", Executors.newSingleThreadExecutor(), mCallback)
+        val mRequest = builder.build()
+        mRequest.start()
     }
 
     @Test
@@ -79,4 +92,38 @@ class ZygoteMemoryExperiments {
         context.startService(serviceIntent2)
     }
 }
+
+
+class MyCallback : UrlRequest.Callback {
+        override fun onRedirectReceived(
+                request: UrlRequest, info: UrlResponseInfo, newLocationUrl: String) {
+          Log.i(TAG, "onRedirectReceived")
+          request.followRedirect()
+        }
+
+
+        override fun onResponseStarted(request: UrlRequest, info: UrlResponseInfo) {
+          Log.i(TAG, "onResponseStarted")
+          request.read(ByteBuffer.allocateDirect(1024))
+        }
+
+        override fun onReadCompleted(
+                request: UrlRequest, info: UrlResponseInfo, byteBuffer: ByteBuffer) {
+          Log.i(TAG, "onReadCompleted")
+          request.read(ByteBuffer.allocateDirect(1024))
+        }
+
+        override fun onSucceeded(request: UrlRequest, info: UrlResponseInfo) {
+          Log.i(TAG, "onReadCompleted")
+        }
+
+        override fun onFailed(
+                request: UrlRequest, info: UrlResponseInfo?, error: HttpException) {
+          Log.i(TAG, "onFailed")
+        }
+
+        override fun onCanceled(request: UrlRequest, info: UrlResponseInfo?) {
+          Log.i(TAG, "onCanceled")
+        }
+    }
 
