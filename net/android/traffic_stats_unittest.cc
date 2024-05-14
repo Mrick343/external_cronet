a1f0f47789264e18e851a5c3116c75fd550c1da8
Junyu Lai <junyulai@google.com>
2024-05-14 16:53:24 +0800

    Fix cronet unit test for TrafficStats cache mechanism
    Currently, a rate-limit on TrafficStats getter APIs to
    reduce the need of accessing bpf map. For any caller who calls
    this method within 1 second, a stale result will be returned.
    
    One expected outcome is this change would break any test which
    relies on immediate change of the result of TrafficStats after
    making network traffic. Thus, related tests are needed to be
    fixed accordingly.
    
    This change do retries when getting readings from TrafficStats,
    to add tolerance of not getting immediate change of TrafficStats.
    Up to 1 second delay to complete the affected tests are expected.
    
    Test: atest net_unittests_tester
    Bug: 339154075
    Change-Id: I76c0c1da66376bc4db614eb5443e0b6f7618a53b

--- net/android/traffic_stats_unittest.cc
+++ net/android/traffic_stats_unittest.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <unistd.h>     // For usleep
+
 #include "net/android/traffic_stats.h"
 
 #include "base/run_loop.h"
@@ -16,6 +18,34 @@
 namespace net {
 
 namespace {
+
+
+// Function template for flexibility in the expect_func type
+template <typename Function>
+void expectGtWithRetry(Function expect_func,
+                       int val_base,
+                       int max_retries = 10,
+                       int retry_interval_ms = 500) {
+    int retry_count = 0;
+    int val_expect;
+
+    while (retry_count < max_retries) {
+        // Execute the provided function to get the value
+        val_expect = expect_func();
+
+        if (val_expect > val_base) {
+            return;  // Success: val1 is greater than val2
+        }
+
+        retry_count++;
+        if (retry_count <= max_retries) {
+            usleep(retry_interval_ms * 1000);
+        }
+    }
+
+    // If reached here, all retries have failed.
+    FAIL();
+}
 
 TEST(TrafficStatsAndroidTest, BasicsTest) {
   base::test::TaskEnvironment task_environment(
@@ -45,12 +75,18 @@ TEST(TrafficStatsAndroidTest, BasicsTest) {
   base::RunLoop().Run();
 
   // Bytes should increase because of the network traffic.
-  int64_t tx_bytes_after_request = -1;
-  int64_t rx_bytes_after_request = -1;
-  EXPECT_TRUE(android::traffic_stats::GetTotalTxBytes(&tx_bytes_after_request));
-  EXPECT_GT(tx_bytes_after_request, tx_bytes_before_request);
-  EXPECT_TRUE(android::traffic_stats::GetTotalRxBytes(&rx_bytes_after_request));
-  EXPECT_GT(rx_bytes_after_request, rx_bytes_before_request);
+  // Retry is needed to avoid rate-limit caching for
+  // TrafficStats API results on V+ devices.
+  expectGtWithRetry([]() -> int {
+    int64_t ret = -1;
+    EXPECT_TRUE(android::traffic_stats::GetTotalTxBytes(&ret));
+    return ret;
+  }, tx_bytes_before_request);
+  expectGtWithRetry([]() -> int {
+      int64_t ret = -1;
+      EXPECT_TRUE(android::traffic_stats::GetTotalRxBytes(&ret));
+      return ret;
+    }, rx_bytes_before_request);
 }
 
 TEST(TrafficStatsAndroidTest, UIDBasicsTest) {
@@ -81,14 +117,18 @@ TEST(TrafficStatsAndroidTest, UIDBasicsTest) {
   base::RunLoop().Run();
 
   // Bytes should increase because of the network traffic.
-  int64_t tx_bytes_after_request = -1;
-  int64_t rx_bytes_after_request = -1;
-  EXPECT_TRUE(
-      android::traffic_stats::GetCurrentUidTxBytes(&tx_bytes_after_request));
-  EXPECT_GT(tx_bytes_after_request, tx_bytes_before_request);
-  EXPECT_TRUE(
-      android::traffic_stats::GetCurrentUidRxBytes(&rx_bytes_after_request));
-  EXPECT_GT(rx_bytes_after_request, rx_bytes_before_request);
+  // Retry is needed to avoid rate-limit caching for
+  // TrafficStats API results on V+ devices.
+  expectGtWithRetry([]() -> int {
+      int64_t ret = -1;
+      EXPECT_TRUE(android::traffic_stats::GetTotalTxBytes(&ret));
+      return ret;
+    }, tx_bytes_before_request);
+   expectGtWithRetry([]() -> int {
+       int64_t ret = -1;
+       EXPECT_TRUE(android::traffic_stats::GetTotalRxBytes(&ret));
+       return ret;
+  }, rx_bytes_before_request);
 }
 
 }  // namespace
