0d465c576d5ac2e8116530b95cca53b75a6c61e1
Motomu Utsumi <motomuman@google.com>
2024-05-30 15:12:53 +0900

    Cronet 122.0.6261.43: Fix merge conflict in NativeTestServer.java
    aosp/3104740 missed to fix the merge conflicts
    
    Test: m NetHttpCoverageTests
    Bug: 342972034
    Change-Id: I083b7b35b3f755928a98924c77d685581ea7510e

--- components/cronet/android/test/src/org/chromium/net/NativeTestServer.java
+++ components/cronet/android/test/src/org/chromium/net/NativeTestServer.java
@@ -113,10 +113,6 @@ public final class NativeTestServer {
                 String testDataDir,
                 boolean useHttps,
                 @ServerCertificate int certificate);
-<<<<<<< HEAD
-=======
-
->>>>>>> upstream-import
         void shutdownNativeTestServer();
 
         String getEchoBodyURL();
